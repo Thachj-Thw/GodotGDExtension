d12b5108035591c95dbe44d698c36edadb5d7ecb
Thachj-Thw <chanhosatoh@gmail.com>
2024-09-08 16:08:17 +0700

    edit register_type

--- windowhandle/src/register_types.h
+++ windowhandle/src/register_types.h
@@ -1,11 +1,11 @@
-#ifndef SCREENSHOT_REGISTER_TYPES_H
-#define SCREENSHOT_REGISTER_TYPES_H
+#ifndef HANDLEWINDOW_REGISTER_TYPES_H
+#define HANDLEWINDOW_REGISTER_TYPES_H
 
 #include <godot_cpp/core/class_db.hpp>
 
 using namespace godot;
 
-void initialize_screenshot_module(ModuleInitializationLevel p_level);
-void uninitialize_screenshot_module(ModuleInitializationLevel p_level);
+void initialize_handlewindow_module(ModuleInitializationLevel p_level);
+void uninitialize_handlewindow_module(ModuleInitializationLevel p_level);
 
-#endif // SCREENSHOT_REGISTER_TYPES_H
+#endif // HANDLEWINDOW_REGISTER_TYPES_H
